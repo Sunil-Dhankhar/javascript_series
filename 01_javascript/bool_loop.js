b7d93d25174c656304b3f978f5556b2aeb4cfe3a
Skool Of Code By  Sunil Dhankhar <sunilkumardhankhar@gmail.com>
2025-07-01 12:20:23 +0000

    bool file updated

---
+++ 01_javascript/bool_loop.js
@@ -0,0 +1,44 @@
+/* JavaScript Booleans */
+// The Boolean Data Type
+// In JavaScript, a Boolean is a primitive data type that can only have one of two values : true or false.
+// The Boolean value of an expression is the basis for all JavaScript comparisons and conditions.
+        // {
+            // true and false are boolean data types
+            // true and false are the only possible boolean values
+            // true and false must be written in lowercase
+            // true and false must be written without quotes
+        //}
+// Boolean Use Cases
+    // {
+        // yes or no
+        // on or off
+        // true or false
+    // }
+
+
+    let x = 5; 
+    console.log(x==10); // check or verify x is equal to 10 if equal then return true else return false as boolearn
+
+    /* Conditional Statement  */
+    
+    if(x > 10) // if else boolearn condition that check x is large then 10 then permorm this scope functionality
+    {
+        msg = x+" is bigger"
+    }
+    else // other wise run this scope script functionality..
+    {
+        msg = x + " is smaller"
+    }
+    console.log(msg);// print the scope functioinality based on the condition
+
+    
+    /* Loops */
+
+    //while loop
+    let text = "";
+    let i = 1;
+    while(i <= x) {
+        text += i;
+        i++;
+    }
+    console.log(text);
