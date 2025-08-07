a2708f0c9aba21ea8bd2d84ba055c429e19d4e9a
Sunil Dhankhar <sunilkumardhankhar@gmail.com>
2025-08-07 15:04:35 +0530

    some methods are performed under the lodash library

--- js_libraries/lodash.js
+++ js_libraries/lodash.js
@@ -5,13 +5,13 @@
 // this Library is more powerfull that works with Arrays , Objects, strings, functions and collection for create your code easy and clean for faster execution.
 
 
-const _ = require('lodash', 'underscore');
+const _ = require('lodash');
 {
     // let arr = [1, 2, 2, 3, 4]; define a new array with values
     let arr =  { name: "John", age: function () {return 30;}, city: "New York", city:"Jalandhar",name:"smith", fun:"age"}; // define a object
     //let arr = "Lodash is a Utility Library that is used to create a user friendly code"; // define a string
-    console.log(_.uniq(arr), 1); // get unique value from the array values. and ignore the duplicate value. 
-    console.log(_.uniq(Object.entries(arr)), 1); // get the uniqe value from the object after converting object to array.
+   // console.log(_.uniq(arr), 1); // get unique value from the array values. and ignore the duplicate value. 
+    console.log(_.uniq(Object.entries(arr))); // get the uniqe value from the object after converting object to array.
     console.log(_.chunk(Object.entries(arr))); // get the chunks (parts/pieces) of object array (object after converting object to array).
     console.log(_.flatten(_.chunk(Object.entries(arr)))); //get a single array with all values from nesting array to single array
 }
@@ -54,7 +54,8 @@ const _ = require('lodash', 'underscore');
     console.log(_.dropRight(arr1, 3));// output => [ 1, 2, 2, 3 ]
     console.log(_.dropRightWhile(arr1, n => n >= 8)); // output => [ 1, 2, 2, 3, 4 ] // with this example  you see in this condition remove all value that are  greater and equals to 8 from the array. 
     //console.log(_.dropRightWhile(arr, (user) => user.active === false)); // output => [ { user: 'Hardy', active: true } ] // with this example  you see in this condition remove all value that are equals to false from the array. 
-    console.log(_.dropWhile(arr, (user) => user.active !== false));    //[{user:'Ammy', active: false }, { user: 'Diljeet', active: false } ] // in this example you see that while the condition is true it removes the elements from the array and when the condition is false it stops and exit from the loop and returns all remained array as data.
+    console.log(_.dropWhile(arr, (user) => user.active === true));    //[{user:'Ammy', active: false }, { user: 'Diljeet', active: false } ] // in this example you see that while the condition is true it removes the elements from the array and when the condition is false it stops and exit from the loop and returns all remained array as data.
+    // this return the object value from where the condition is become false, and when condition is false then it returns the remaining objects.
 }
 
 { // _.functions(object) => this method is used to get all function name from the object. means it returns all methods name from a perticuler object. this method returns only owns method not parent or inherit function property or name.
@@ -62,7 +63,7 @@ const _ = require('lodash', 'underscore');
         name : "Spity vally", // properties of main object 
         locs : "Himachal Pardesh, India" ,
         pack_price:14999+"/-",
-        extra : function (){ // declare a function inside object property for perform more functionality when object has been called
+        extra: function (){ // declare a function inside object property for perform more functionality when object has been called
             return `I love the ${this.name} Located in ${this.locs} and the Package Price is ${this.pack_price} Only`; // return string when the object function is called
         }
     }
@@ -74,15 +75,32 @@ const _ = require('lodash', 'underscore');
     const props = { // declare an objects with its property
         name: "Leh-Ladakh",
         locs: "Ladakh (UT, J&K), India ",
-        allowed:()=>{ return "In Summers Only, In Winter its Off.";} // added a new arrow function inside the object property
+        price:13999+"/-",
+        exists : function(){ return this.name, this.locs, this.price;},
+        allowed: function (){ return "In Summers Only, In Winter its Off.";} // added a new arrow function inside the object property
     };
-    let result = _.create(trek, props)
-    let single = _.functions(props);
-    let multi = _.functionsIn(result);
+    let result = _.create(trek, props) // this method is used to create a new object that having object prototype with another object additional properties.
+    let single = _.functions(props); // this method is used to returns object own functions and objects in form of array. // ['allowed']
+    let multi = _.functionsIn(result); // this method is used to returns functions and methods from the prototype object along with its own and return into form of array. ['allowed', 'extra']
     console.log(single); // output => [ 'allowed' ];
     console.log(multi); // output => [ 'allowed', 'extra'];
+    // till here the function method is here provide the each object own function with 
+    
+    // these example are basically we keep here for understand that what are happend here in above methods.
+    console.log(trek.extra()); // output => I love the Spity vally Located in Himachal Pardesh, India and the Package Price is 14999/- Only
+    // here you see that when we use the simplest way to print object properties along with its mentioned function then this provide / Return Object own properties and values.  
+    
+    console.log(result.extra()); // output => I love the Leh-Ladakh Located in Ladakh (UT, J&K), India  and the Package Price is 14999/- Only
+    // in this example you see that the result is a prototype object (trek + props) and when we fetch / extract its property then you see that the if some properties one object have missing 
+    // then it access these properties from the other related or linked object like prototype or enumerable object. 
+    // you see the trek object have a one differnt property that call inside its own function. and this property not exist in props object so here is a benifit of prototype chaining that both object are use each property that they have not in own bucket.
+    // here you see that when we call the prototype object with its enumerable object, then enumerable object have not pack_price property so when the call is created for new created object then the prototype allowed to enumerable object access its own property fir itself that not exist in enumerable object
 
-
+    console.log(result.allowed()); // Output => In Summers Only, In Winter its Off.
+    // when we refer a function from the enumerable object then this return current refered function value without the own object value and prototype object existing values. means this ignore the common value from both object and return the unique value from the enumerable object.
+    // this condition clearify in bottom example where common properties are ignored and only unique properties are print or transferedor somthing else according to your requirement,
+    //  when we call enumerable object method, this print only unique values from the enumerable (calling function object) properties.
+    console.log(result.exists()); // output => 13999/-
 }
 
 { // _.toPath(string path) => this method is used to convert the path of object value from string to array value. this method is used in mainly nested objects for access the object value easily.
@@ -98,7 +116,12 @@ const _ = require('lodash', 'underscore');
     let result = rest.reduce((obj, keys) => { // reduce function is here reducing the multiple value into a single value and then after return the single value.
             return obj && obj[keys]// return the value after match the value. with condition
     },car); // from object // object name
-    console.log(result); //print the returned back result as output.
+    console.log(result); //print the returned back result as output. // Black
+
+    let chaining = _.chain(arras).toPath().reduce((obj ,keys)=>{return obj && obj[keys]}, car).value();
+    // here we have applied a chain method for wrap a object and then applied function or method on this object in chain system  
+    // each method is applied on object one by one in loop or chain. and after each method process the value method unwraped it and retrun the value.
+    console.log(chaining); // output => Black
 }
 
 
